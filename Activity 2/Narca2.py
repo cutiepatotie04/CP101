f52875fc6416e2a462dd70110a0d396f1c745e2c
narca.py <vmae308@gmail.com>
2024-09-29 13:24:28 +0800

    Narca2.py

---
+++ Activity 2/Narca2.py
@@ -0,0 +1,9 @@
+#ACTIVITY 2
+ 
+grade_10 = int(input("HOPE: "))
+grade_11 = int(input("ENGLISH: "))
+grade_12 = int(input("MATH "))
+ 
+average = (grade_10 + grade_11 + grade_12)/3
+ 
+print("AVERAGE: " + str(average))
